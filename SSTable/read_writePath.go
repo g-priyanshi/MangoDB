1543421a5213d302863fca0f592ebe49fdcf327b
dwsds <146217248+dwsds@users.noreply.github.com>
2025-04-10 21:00:05 +0530

    Update read&write

---
+++ SSTable/read_writePath.go
@@ -0,0 +1,307 @@
+package sstable
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"hash/crc32"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"sort"
+)
+
+// Entry represents a key-value pair in the SSTable
+type Entry struct {
+	Key   string
+	Value string
+}
+
+// DataBlock is a sorted block of key-value entries
+type DataBlock struct {
+	Entries []Entry
+}
+
+func (db *DataBlock) Encode() []byte {
+	buf := new(bytes.Buffer)
+	for _, entry := range db.Entries {
+		binary.Write(buf, binary.LittleEndian, int32(len(entry.Key)))
+		buf.Write([]byte(entry.Key))
+		binary.Write(buf, binary.LittleEndian, int32(len(entry.Value)))
+		buf.Write([]byte(entry.Value))
+	}
+	return buf.Bytes()
+}
+
+// IndexEntry points to the start of a data block in the file
+type IndexEntry struct {
+	Key    string
+	Offset int64
+}
+
+// FilterBlock - a basic Bloom filter for demonstration (not optimal)
+type FilterBlock struct {
+	Filter map[string]bool
+}
+
+func (fb *FilterBlock) Add(key string) {
+	fb.Filter[key] = true
+}
+
+func (fb *FilterBlock) MightContain(key string) bool {
+	return fb.Filter[key]
+}
+
+// SSTableWriter writes sorted key-value pairs to an SSTable
+
+func getSSTableFilename(index int) string {
+	return "sstable_" + fmt.Sprintf("%d", index) + ".sst"
+}
+
+func writeSingleSSTable(filename string, entries []Entry, blockSize int) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	index := []IndexEntry{}
+	filter := FilterBlock{Filter: make(map[string]bool)}
+
+	offset := int64(0)
+	block := DataBlock{}
+	for i, entry := range entries {
+		block.Entries = append(block.Entries, entry)
+		filter.Add(entry.Key)
+		if len(block.Entries) == blockSize || i == len(entries)-1 {
+			blockData := block.Encode()
+			checksum := crc32.ChecksumIEEE(blockData)
+			file.Write(blockData)
+			binary.Write(file, binary.LittleEndian, checksum)
+			index = append(index, IndexEntry{Key: block.Entries[0].Key, Offset: offset})
+			offset += int64(len(blockData) + 4)
+			block.Entries = nil
+		}
+	}
+
+	indexOffset := offset
+	for _, ie := range index {
+		binary.Write(file, binary.LittleEndian, int32(len(ie.Key)))
+		file.Write([]byte(ie.Key))
+		binary.Write(file, binary.LittleEndian, ie.Offset)
+	}
+
+	footer := make([]byte, 8)
+	binary.LittleEndian.PutUint64(footer, uint64(indexOffset))
+	file.Write(footer)
+
+	return nil
+}
+
+func getNextSSTableIndex(baseFilename string) int {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		return 0
+	}
+	pattern := fmt.Sprintf(`^%s_(\d+)\.sst$`, baseFilename)
+	re := regexp.MustCompile(pattern)
+	maxIndex := -1
+	for _, file := range files {
+		match := re.FindStringSubmatch(file.Name())
+		if len(match) > 1 {
+			var idx int
+			fmt.Sscanf(match[1], "%d", &idx)
+			if idx > maxIndex {
+				maxIndex = idx
+			}
+		}
+	}
+	return maxIndex + 1
+}
+
+func WriteSSTables(baseFilename string, entries []Entry, blockSize int, entriesPerSSTable int) error {
+	// Sort entries by key
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].Key < entries[j].Key
+	})
+
+	// Get the next available SSTable index
+	sstableIndex := getNextSSTableIndex(baseFilename)
+
+	// Write SSTables in chunks
+	for i := 0; i < len(entries); i += entriesPerSSTable {
+		end := i + entriesPerSSTable
+		if end > len(entries) {
+			end = len(entries)
+		}
+		subEntries := entries[i:end]
+
+		filename := fmt.Sprintf("%s_%d.sst", baseFilename, sstableIndex)
+		err := writeSingleSSTable(filename, subEntries, blockSize)
+		if err != nil {
+			return err
+		}
+		sstableIndex++
+	}
+	return nil
+}
+
+func decodeDataBlock(data []byte) ([]Entry, error) {
+	var entries []Entry
+	buf := bytes.NewReader(data)
+	for buf.Len() > 0 {
+		var keyLen int32
+		if err := binary.Read(buf, binary.LittleEndian, &keyLen); err != nil {
+			return nil, err
+		}
+		keyBytes := make([]byte, keyLen)
+		if _, err := buf.Read(keyBytes); err != nil {
+			return nil, err
+		}
+		var valLen int32
+		if err := binary.Read(buf, binary.LittleEndian, &valLen); err != nil {
+			return nil, err
+		}
+		valBytes := make([]byte, valLen)
+		if _, err := buf.Read(valBytes); err != nil {
+			return nil, err
+		}
+		entries = append(entries, Entry{Key: string(keyBytes), Value: string(valBytes)})
+	}
+	return entries, nil
+}
+
+func ReadAllTables(baseFilename string) ([]Entry, error) {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		return nil, err
+	}
+
+	pattern := fmt.Sprintf(`^%s_(\d+)\.sst$`, baseFilename)
+	re := regexp.MustCompile(pattern)
+
+	var allEntries []Entry
+
+	for _, file := range files {
+		if !re.MatchString(file.Name()) {
+			continue
+		}
+
+		data, err := os.ReadFile(file.Name())
+		if err != nil {
+			return nil, err
+		}
+
+		// Read footer (last 8 bytes)
+		if len(data) < 8 {
+			continue
+		}
+		indexOffset := binary.LittleEndian.Uint64(data[len(data)-8:])
+		indexData := data[indexOffset : len(data)-8]
+
+		// Read index
+		var index []IndexEntry
+		buf := bytes.NewReader(indexData)
+		for buf.Len() > 0 {
+			var keyLen int32
+			if err := binary.Read(buf, binary.LittleEndian, &keyLen); err != nil {
+				break
+			}
+			key := make([]byte, keyLen)
+			if _, err := buf.Read(key); err != nil {
+				break
+			}
+			var offset int64
+			if err := binary.Read(buf, binary.LittleEndian, &offset); err != nil {
+				break
+			}
+			index = append(index, IndexEntry{Key: string(key), Offset: offset})
+		}
+
+		// Read data blocks
+		for i, idx := range index {
+			start := idx.Offset
+			var end int64
+			if i+1 < len(index) {
+				end = index[i+1].Offset
+			} else {
+				end = int64(indexOffset)
+			}
+			blockData := data[start : end-4] // exclude checksum
+			entries, err := decodeDataBlock(blockData)
+			if err != nil {
+				return nil, err
+			}
+			allEntries = append(allEntries, entries...)
+		}
+	}
+
+	return allEntries, nil
+}
+
+func ReadValueByKey(baseFilename string, key string) (string, bool, error) {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		return "", false, err
+	}
+
+	pattern := fmt.Sprintf(`^%s_(\d+)\.sst$`, baseFilename)
+	re := regexp.MustCompile(pattern)
+
+	for _, file := range files {
+		if !re.MatchString(file.Name()) {
+			continue
+		}
+
+		data, err := os.ReadFile(file.Name())
+		if err != nil {
+			return "", false, err
+		}
+
+		if len(data) < 8 {
+			continue
+		}
+		indexOffset := binary.LittleEndian.Uint64(data[len(data)-8:])
+		indexData := data[indexOffset : len(data)-8]
+
+		var index []IndexEntry
+		buf := bytes.NewReader(indexData)
+		for buf.Len() > 0 {
+			var keyLen int32
+			if err := binary.Read(buf, binary.LittleEndian, &keyLen); err != nil {
+				break
+			}
+			keyBytes := make([]byte, keyLen)
+			if _, err := buf.Read(keyBytes); err != nil {
+				break
+			}
+			var offset int64
+			if err := binary.Read(buf, binary.LittleEndian, &offset); err != nil {
+				break
+			}
+			index = append(index, IndexEntry{Key: string(keyBytes), Offset: offset})
+		}
+
+		// Binary search in index for potential block
+		for i, idx := range index {
+			start := idx.Offset
+			var end int64
+			if i+1 < len(index) {
+				end = index[i+1].Offset
+			} else {
+				end = int64(indexOffset)
+			}
+			blockData := data[start : end-4] // exclude checksum
+			entries, err := decodeDataBlock(blockData)
+			if err != nil {
+				return "", false, err
+			}
+			for _, entry := range entries {
+				if entry.Key == key {
+					return entry.Value, true, nil
+				}
+			}
+		}
+	}
+	return "", false, nil
+}
